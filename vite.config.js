72ab7451fb1d34a1a0443707d32deaf941ff185b
d.muxametshina <d.muxametshina@kapitalbank.uz>
2024-10-11 14:34:52 +0500

    check nginx

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,5 @@ export default defineConfig({
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_PROD_DEVTOOLS__: false
-  },
-  build: {
-    outDir: '/var/www/html',
-    emptyOutDir: true
   }
 })
